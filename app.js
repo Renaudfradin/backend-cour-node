04c7a256da43777882690e185a7d8cd41c3f63b8
Renaud <renaudfradin@gmail.com>
2020-11-23 12:32:53 +0100

    lire ecrire update and delete data

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const mongoose = require('mongoose');
+const Thing = require('./models/Thing.js')
+
+mongoose.connect('mongodb+srv://Renaud:Tigrette179236502017@cluster0.yiuvc.mongodb.net/projecttest?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true})
+ .then(()=>console.log('connexion reussie'))
+ .catch(()=>console.log('connexion echoue'));
 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,26 +15,41 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.use('/api/stuff',(req,res,next)=>{
-    const stuff = [
-        {
-          _id: 'oeihfzeoi',
-          title: 'Mon premier objet',
-          description: 'Les infos de mon premier objet',
-          imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-          price: 4900,
-          userId: 'qsomihvqios',
-        },
-        {
-          _id: 'oeihfzeomoihi',
-          title: 'Mon deuxième objet',
-          description: 'Les infos de mon deuxième objet',
-          imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-          price: 2900,
-          userId: 'qsomihvqios',
-        },
-      ];
-      res.status(200).json(stuff);
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff',(req,res,next)=>{
+    delete req.body._id;
+    const thing = new Thing ({
+      ...req.body
+    });
+    thing.save()
+      .then(()=> res.status(201).json({ message:"requette envoyer"}))
+      .catch(error => res.status(400).json({error}));
+});
+
+app.delete('/api/stuff/:id',(req,res,next)=>{
+  Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message : "objet delete"}))
+      .catch(error => res.status(400).json({error}));
+})
+
+app.put('/api/stuff/:id',(req,res,next)=>{
+  Thing.updateOne({ _id: req.params.id },{ ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message : "objet mofifier"}))
+      .catch(error => res.status(400).json({error}));
+})
+
+app.get('/api/stuff/:id',(req,res,next)=>{
+  Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({error}));
+})
+
+app.get('/api/stuff',(req,res,next)=>{
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({error}));
 })
 
 module.exports = app;
